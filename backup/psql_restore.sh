a407149c8defe770ba7462f14730ba987467d716
Ernad Husremovic <hernad@bring.out.ba>
2011-12-07 15:19:03 +0100

    rm ne radi, 0.4.0 ver

--- backup/psql_restore.sh
+++ backup/psql_restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# ver 0.3
-# bjasko@bring.out.ba 
+# ver 0.4.0
+# bjasko@bring.out.ba, hernad@bring.out.ba
 # 07.12.2011
 # pozvi ./psql_restore.sh [hostname] [username] [dbname]  [filename]
 #############################
@@ -20,10 +20,8 @@ else
 fi
  
 
-cd $BCKPDIR
-
 echo ""
-echo "raspakiram tgz"
+echo "untarujem $BCKPDIR/$BACK_FILE"
 echo ""
 
 
@@ -33,12 +31,10 @@ tar xvfz $BACK_FILE
 echo " PSQL restore........unesi $PSQLUSER PWD:"
 
 
-pg_restore --host $PSQLHOST --username $PSQLUSER -W --dbname="$3"  $PSQLFILE 
+pg_restore --host $PSQLHOST --username $PSQLUSER -W --dbname="$3"  $BCKPDIR/$PSQLFILE 
 
 
 echo " Restore iz dumpa  $BACKFILE u $1 $3 zavrsen ........"
 rm $BCKPDIR/$PSQLFILE
 
 exit 0
-
-
