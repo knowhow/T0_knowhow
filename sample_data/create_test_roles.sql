e311f23e692df6105718bdb9e5c9b0721c4e308e
Ernad Husremovic <hernad@bring.out.ba>
2011-12-22 20:34:03 +0100

    create test roles

---
+++ sample_data/create_test_roles.sql
@@ -0,0 +1,5 @@
+create role xtrole;
+create role admin password 'admin' login in group xtrole;
+create role test1 password 'test1' login in group xtrole;
+create role test2 password 'test2' login in group xtrole;
+create role test3 password 'test3' login in group xtrole;
