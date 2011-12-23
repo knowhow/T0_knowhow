908795d6a6994dbd7d78878f3dbda85948250d8a
Jasmin Beganović <bjasko@bring.out.ba>
2011-12-23 21:59:19 +0100

    pg daily backup

---
+++ backup/pg_daily_backup.bat
@@ -0,0 +1,50 @@
+@echo off
+echo ===========================================================================
+echo Ovaj batch file backupira postgresql DB  na lokaciju c:\knowhowERP\backup
+echo ver: 0.1.0  23.12.2011
+echo bjasko@bring.out.ba   
+echo ===========================================================================
+set hostname=localhost
+set pgusername=admin
+set PGPASSWORD=admin
+set pgdbname=bringout_test
+set orgname=rudnik1
+for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
+     set dow=%%i
+     set month=%%j
+     set day=%%k
+     set year=%%l
+ )
+set datestr=%month%_%day%_%year%
+echo datestr is %datestr%
+    
+set backup_file=%orgname%_%datestr%.backup
+echo backup je  %backup_file%
+echo on
+pg_dump -i -h %hostname%  -p 5432 -U %pgusername% -F c -b -v -f %backup_file%  %pgdbname%
+echo gzipujem max kompresija 
+gzip -cN9 %backup_file%  > %backup_file%.gz
+gzip  -tv  %backup_file%.gz
+if errorlevel 1 goto ERR
+if errorlevel 0 goto OK 
+
+:OK   
+del %backup_file%
+exit
+
+:ERR
+cls
+color 4f
+echo.
+echo. 
+echo.
+echo.
+echo BACKUP NEUSPJESAN !!!!!!!!!!!!!!!!!!!
+echo KONTAKTIRATI BRING.OUT SERVIS
+echo.
+echo.
+echo.
+echo. 
+pause 
+
+
