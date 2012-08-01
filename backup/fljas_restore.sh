eee0d351b25fd92829d21f43201e2119dec76738
Jasmin Beganović <bjasko@bring.out.ba>
2012-08-01 15:48:07 +0200

    fljas restore tar extr ispravke

--- backup/fljas_restore.sh
+++ backup/fljas_restore.sh
@@ -4,21 +4,21 @@
 # backupi idu u  $1 
 
 #system settings 
-tar -C / xfvz  $1/network.tar.gz  
+tar xfvz  $1/network.tar.gz  
 cp -a $1/hosts /etc/hosts  
 cp -a $1/hostname  /etc/hostname  
 cp -a $1/rc.local  /etc/rc.local 
 
 #user files/settings
-tar -C / xfvz $1/config.tar.gz 
-tar -C / xfvz $1/pidgin.tar.gz 
+tar  xfvz $1/config.tar.gz 
+tar  xfvz $1/pidgin.tar.gz 
 
 ##folders
-tar -C / xfvz $1/tops.tar.gz 
-tar -C / xfvz $1/sigma.tar.gz 
-tar -C / xfvz $1/wine_tremol.tar.gz 
-tar -C / xfvz $1/dosemu.tar.gz 
-tar -C / xfvz $1/wine.tar.gz 
+tar  xfvz $1/tops.tar.gz 
+tar  xfvz $1/sigma.tar.gz 
+tar  xfvz $1/wine_tremol.tar.gz 
+tar  xfvz $1/dosemu.tar.gz 
+tar  xfvz $1/wine.tar.gz 
 
 
 exit
