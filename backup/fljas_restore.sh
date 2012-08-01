3f0ceaffdcc7ccfb7d7f3ed02488e5a76ddcc2af
Jasmin Beganović <bjasko@bring.out.ba>
2012-08-01 15:41:49 +0200

    fljas restore init

---
+++ backup/fljas_restore.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# ver 1.0
+# bjasko@bring.out.ba
+# backupi idu u  $1 
+
+#system settings 
+tar -C / xfvz  $1/network.tar.gz  
+cp -a $1/hosts /etc/hosts  
+cp -a $1/hostname  /etc/hostname  
+cp -a $1/rc.local  /etc/rc.local 
+
+#user files/settings
+tar -C / xfvz $1/config.tar.gz 
+tar -C / xfvz $1/pidgin.tar.gz 
+
+##folders
+tar -C / xfvz $1/tops.tar.gz 
+tar -C / xfvz $1/sigma.tar.gz 
+tar -C / xfvz $1/wine_tremol.tar.gz 
+tar -C / xfvz $1/dosemu.tar.gz 
+tar -C / xfvz $1/wine.tar.gz 
+
+
+exit
+
