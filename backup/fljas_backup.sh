fdd9d6ca95b8cf1b0712b032595f2a20ec06649f
Jasmin Beganović <bjasko@bring.out.ba>
2012-08-01 11:57:04 +0200

    fljas_backup init

---
+++ backup/fljas_backup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# ver 1.0
+# bjasko@bring.out.ba
+# backupi idu u  /var/backups/vp/
+DATETIME=`date +%Y%m%d`
+USER="bringout"
+DEST="/var/backups/vp/$DATETIME"
+
+# kreiraj DEST
+mkdir -p $DEST
+
+#system settings 
+tar cfvz  $DEST/network.tar.gz   /etc/NetworkManager/
+cp -a /etc/hosts  $DEST
+cp -a /etc/hostname $DEST 
+cp -a /etc/rc.local $DEST 
+
+#user files/settings
+tar cfvz $DEST/config.tar.gz /home/$USER/.config/
+tar cfvz $DEST/pidgin.tar.gz /home/$USER/.purple/
+
+##folders
+tar cfvz $DEST/tops.tar.gz /home/$USER/tops/
+tar cfvz $DEST/sigma.tar.gz /home/$USER/sigma/
+tar cfvz $DEST/wine_tremol.tar.gz /home/$USER/.wine_tremol/
+tar cfvz $DEST/dosemu.tar.gz /home/$USER/.dosemu/
+tar cfvz $DEST/wine.tar.gz /home/$USER/.wine/
+
+
+exit
+
