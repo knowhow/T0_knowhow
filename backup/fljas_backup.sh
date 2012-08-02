b7c287ef298d71f47491425e4d10d71565f916e0
Jasmin Beganović <bjasko@bring.out.ba>
2012-08-02 08:27:38 +0200

    fljas_backup purple, dosemu

--- backup/fljas_backup.sh
+++ backup/fljas_backup.sh
@@ -12,6 +12,8 @@ mkdir -p $DEST
 #system settings 
 tar cfvz  $DEST/network.tar.gz   /etc/NetworkManager/
 tar cfvz  $DEST/vpn.tar.gz   /etc/openvpn/
+tar cfvz  $DEST/dosemu.tar.gz   /etc/dosemu/
+tar cfvz  $DEST/purple.tar.gz   /etc/purple/
 cp -a /etc/hosts  $DEST
 cp -a /etc/hostname $DEST 
 cp -a /etc/rc.local $DEST 
@@ -19,8 +21,9 @@ cp -a /etc/rc.local $DEST
 #user files/settings
 tar cfvz $DEST/config.tar.gz /home/$USER/.config/
 tar cfvz $DEST/pidgin.tar.gz /home/$USER/.purple/
+cp -a /home/$USER/.dosemurc  $DEST
 
-##folders
+# home folders
 tar cfvz $DEST/tops.tar.gz /home/$USER/tops/
 tar cfvz $DEST/sigma.tar.gz /home/$USER/sigma/
 tar cfvz $DEST/wine_tremol.tar.gz /home/$USER/.wine_tremol/
