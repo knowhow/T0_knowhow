c0272bbac1969f11bdd88b2db49b1326862ac12b
Jasmin Beganović <bjasko@bring.out.ba>
2012-08-02 08:42:12 +0200

    fljas_restore  purple, dosemu

--- backup/fljas_restore.sh
+++ backup/fljas_restore.sh
@@ -7,6 +7,8 @@
 #system settings 
 tar xfvz  $1/network.tar.gz -C / 
 tar xfvz  $1/vpn.tar.gz -C /
+tar xfvz  $1/purple.tar.gz -C /
+
 cp -a $1/hosts /etc/hosts  
 cp -a $1/hostname  /etc/hostname  
 cp -a $1/rc.local  /etc/rc.local 
@@ -14,6 +16,7 @@ cp -a $1/rc.local  /etc/rc.local
 #user files/settings
 tar  xfvz $1/config.tar.gz -C /
 tar  xfvz $1/pidgin.tar.gz -C /
+cp -a $1/.dosemurc  /home/bringout/
 
 ##folders
 tar  xfvz $1/tops.tar.gz  -C /
