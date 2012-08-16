c27eb4c818b885090d178b197bfd90dfdbf3af96
Jasmin Beganović <bjasko@bring.out.ba>
2012-08-16 09:06:21 +0200

    knowhow updater backup ispravke

--- backup/update_knowhoERP.sh
+++ backup/update_knowhoERP.sh
@@ -7,33 +7,45 @@ B_DATE=`date "+%d_%m_%Y_%H_%M"`
 
 ARGS=5
 BADARGS=85
-
+PGPASSWORD="$5"
+PGHOSTADDR="$1"
+PGUSER="$4"
 # check args
 
 if [ $# -lt  "$ARGS" ];then
      echo ""
      echo ""
-     echo "Upotreba: vidi http://redmine.bring.out.ba/issues/28527 "
+     echo "Upotreba: vidi http://redmine.bring.out.ba/issues/28899"
      echo "" 
-     echo "./update_knowhowERP.sh localhost port update_file username pwd "
+     echo "./update_knowhowERP.sh localhost port update_file username pwd backup <<< backup je opcionalan vidi #28899 "
      echo ""
      exit $BADARGS
 fi
 
+if [ $# =  "$ARGS" ];then
+     echo "update bez backupa"
+     sleep 5
+fi
 
-
-
-# napravi full backup servera prije update-a baze u /tmp 
-
-su - postgres  -c "/usr/bin/pg_dumpall | gzip -c > /tmp/$B_DATE.all.sql.gz" 
-
+# DB list 
 DB=$(psql -lt | egrep -v 'template[01]' | egrep -v 'postgres' | awk '{print $1}')
 
+# napravi backup  prije update-a baze u /tmp 
+
+backup () {
+for d in $DB 
+         do 
+         pg_dump   $d  | gzip -c > /tmp/$B_DATE.$d.sql.gz
+         echo "backup baze $d je završen, idem dalje"
+done
+}
+$6
 
 for d in $DB
 
 	do
-  	updater -databaseURL=//$1:$2/$d -username=$4 -passwd=$5 -file=$3 -autorun 
+  	updater -databaseURL=//$1:$2/$d -username=$4 -passwd=$5 -file=$3 -autorun
+        echo "update baze $d gotov, idem dalje...." 
 done 
 
 
