3ee10e076a5f062b9d6ecb8394770cc3f5207619
bringout <bringout@ubuntu01.bring.out.ba>
2014-04-15 14:09:12 +0200

    redmine rsync files backup

---
+++ backup/redmine/rsync_redmine_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# ver 0.1 
+# bjasko@bring.out.ba 
+# 15.04.2014
+
+
+local_dir="/var/lib/vz/files/"
+remote_dir="/nas_backup/files/redmine/"
+
+
+redmine_files=$(rsync --log-file=/tmp/redmine_files_rsync.log -avz --delete  $local_dir   $remote_dir)
+if [ $? -eq 0 ]; then  
+    echo success  
+else  
+    cat /tmp/redmine_files_rsync.log | mail -s "redmine_files rsync problem"  "cs@bring.out.ba"
+fi  
+
+exit
