f694b9f02aa11e0d37667497c7a8453e7a3a02c9
bjasko <jasmin.beganovic@bring.out.ba>
2014-05-23 15:19:42 +0200

    F18 backup rewrite
    ubacio backup tekuće sezone na osnovu datuma sa servera

--- backup/knowhowerp_backup.sh
+++ backup/knowhowerp_backup.sh
@@ -7,16 +7,35 @@
 B_DIR=/var/backups/knowhowERP
 B_ARH=/var/backups/knowhowERP/arhiva
 B_ALL=/var/backups/knowhowERP/all
+B_TEKUCA=/var/backups/knowhowERP/tekuca
 B_DATE=`date "+%d_%m_%Y_%H_%M"`
+B_CURR_Y=`date "+%Y"`
+ 
+
 
 # check if destination exist & create if not 
 [ -d $B_DIR ] || mkdir -p $B_DIR && chown postgres:postgres $B_DIR
 [ -d $B_ARH ] || mkdir -p $B_ARH && chown postgres:postgres $B_ARH
 [ -d $B_ALL ] || mkdir -p $B_ALL && chown postgres:postgres $B_ALL
+[ -d $B_TEKUCA ] || mkdir -p $B_TEKUCA && chown postgres:postgres $B_TEKUCA
 
 
-# delete older than n days
-find $B_DIR/*.gz -mtime +30 -exec rm {} \;
+
+backup_tekuca () {
+# list od DB to backup
+DB=$(su - postgres -c "psql -lt" | egrep -v 'template[01]' | awk '{print $1}' | grep $B_CURR_Y 
+)
+for d in $DB
+
+	do
+  	su - postgres -c "/usr/bin/pg_dump $d | gzip -c > $B_TEKUCA/$B_DATE.$d.sql.gz" 
+
+done 
+ 
+}
+
+
+backup_sve  () { 
 
 # list od DB to backup
 DB=$(su - postgres -c "psql -lt" | egrep -v 'template[01]' | awk '{print $1}')
@@ -25,17 +44,13 @@ for d in $DB
 
 	do
   	su - postgres -c "/usr/bin/pg_dump $d | gzip -c > $B_DIR/$B_DATE.$d.sql.gz" 
-done 
-# list DIR 
-cd $B_DIR 
-pwd
-ls -lh 
+ 
+done
 
-# list ARH
-cd $B_ARH 
-pwd
-ls -lh 
+} 
 
+
+backup_arhiva   () {
 # 1st day of the month, archive backup 
 DAY=`date +%d`
 
@@ -45,6 +60,9 @@ if [ $DAY = 30 ]
 
 fi      
 
+}
+
+backup_server  () {
 # 15th day of the month, full dump  
 DAY=`date +%d`
 
@@ -52,6 +70,53 @@ if [ $DAY = 15 ]
         then
         su - postgres  -c "/usr/bin/pg_dumpall | gzip -c > $B_ALL/$B_DATE.all.sql.gz" 
 fi
+
+} 
+
+
+cisti_backup_tekuca () {
+
+# delete older than n days
+find $B_TEKUCA/*.gz -mtime +30 -exec rm {} \;
+}
+
+
+
+
+cisti_backup_sve () {
+
+# delete older than n days
+find $B_DIR/*.gz -mtime +30 -exec rm {} \;
+}
+
+
+
+
+if [ "$1" = "tekuca" ];
+then
+    echo "tekuca backup"
+    backup_tekuca
+    backup_arhiva
+    backup_server
+    cisti_backup_tekuca
+    exit
+fi
+
+
+if [ "$1" = "sve" ]
+then
+    echo "backup svega"
+    backup_sve
+    backup_arhiva
+    backup_server
+    cisti_backup_sve
+    exit
+else
+    echo "argument ne valja koristi tekuca ili sve"
+    exit
+fi
+
+
 
 
 # uncomment 2 rsync copy to remote  hosts
